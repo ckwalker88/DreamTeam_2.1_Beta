cc77218538da9ae1fd45b6063f424a62a2309937
Unknown <walkerck@etsu.edu>
2018-02-22 03:27:47 -0500

    Vanilla Project

---
+++ BucBoard/BucBoard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BucBoard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
