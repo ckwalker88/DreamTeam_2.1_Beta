3f6eaa0c260a6296dcc5d950219d9c3c54273e1c
ckwalker88 <walkerck@etsu.edu>
2018-02-22 15:11:45 -0500

    Program.cs was the error, most likely because code is slightly different in 2.0. Error fixed!

--- BucBoard/BucBoard/Program.cs
+++ BucBoard/BucBoard/Program.cs
@@ -14,25 +14,9 @@ namespace BucBoard
 {
     public class Program
     {
-        //public static void Main(string[] args)
-        //{
-        //    BuildWebHost(args).Run();
-        //}
-
-        //public static IWebHost BuildWebHost(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args)
-        //        .UseStartup<Startup>()
-        //        .Build();
-
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
+            var host = BuildWebHost(args);
 
             using (var scope = host.Services.CreateScope())
             {
@@ -45,12 +29,22 @@ namespace BucBoard
                 catch (Exception)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError("An error occurred while seeding the database.");
+                    logger.LogError("An error occured while seeding the database.");
                 }
             }
 
-            host.Run();
+             BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
 
     }
 }
--- BucBoard/BucBoard/Startup.cs
+++ BucBoard/BucBoard/Startup.cs
@@ -27,8 +27,9 @@ namespace BucBoard
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<AuthenticationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("BucBoardDB")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<AuthenticationDbContext>()
