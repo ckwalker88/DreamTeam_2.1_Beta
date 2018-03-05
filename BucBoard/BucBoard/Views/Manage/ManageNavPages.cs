0799dea31f4a6779360dcd995cc5433f68566d0d
Christopher Day <DAYCC1@etsu.edu>
2018-03-05 14:37:36 -0500

    Manage Accounts Announcements V1

--- BucBoard/BucBoard/Views/Manage/ManageNavPages.cs
+++ BucBoard/BucBoard/Views/Manage/ManageNavPages.cs
@@ -13,6 +13,8 @@ namespace BucBoard.Views.Manage
 
         public static string Index => "Index";
 
+        public static string Announcements => "Announcements";
+
         public static string ChangePassword => "ChangePassword";
 
         public static string ExternalLogins => "ExternalLogins";
@@ -20,6 +22,8 @@ namespace BucBoard.Views.Manage
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+
+        public static string AnnouncementsNavClass(ViewContext viewContext) => PageNavClass(viewContext, Announcements);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
 
