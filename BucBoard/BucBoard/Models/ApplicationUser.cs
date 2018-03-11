40b74bd4a4a9baa0884bc653256569dddffede73
ckwalker88 <walkerck@etsu.edu>
2018-03-11 19:23:28 -0400

    Added requested fields to the application user

--- BucBoard/BucBoard/Models/ApplicationUser.cs
+++ BucBoard/BucBoard/Models/ApplicationUser.cs
@@ -15,6 +15,9 @@ namespace BucBoard.Models
         public DateTime? LastLogin { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string ClassShedule { get; set; }
+        public string HeadLine { get; set; }
+        public byte[] ProfilePicture { get; set; }
 
     }
 }
