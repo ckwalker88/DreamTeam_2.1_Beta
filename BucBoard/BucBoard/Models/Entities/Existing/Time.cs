ddb13843b3030f6265c2c60a2402f78df73def32
ckwalker88 <walkerck@etsu.edu>
2018-03-27 20:10:20 -0400

    Update Database, now we need CRUD made

---
+++ BucBoard/BucBoard/Models/Entities/Existing/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BucBoard.Models.Entities.Existing
+{
+    public partial class Time
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime StopTime { get; set; }
+        public string ApplicationUserId { get; set; }
+
+        public AspNetUsers ApplicationUser { get; set; }
+    }
+}
