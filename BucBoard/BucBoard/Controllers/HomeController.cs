9dfebe192312a09d44ac1a11d4836c5caaf0e178
Christopher Day <DAYCC1@etsu.edu>
2018-03-31 13:33:46 -0400

    Huge update regarding email sending. Added Email.cshtml view (form_) and two action result methods in HomeController named Email and Email [POST].

--- BucBoard/BucBoard/Controllers/HomeController.cs
+++ BucBoard/BucBoard/Controllers/HomeController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Mvc;
 using BucBoard.Models;
 using BucBoard.Services.Interfaces;
 using BucBoard.Models.Entities.Existing;
+using Microsoft.AspNetCore.Authorization;
+using BucBoard.Models.ViewModels;
+using System.Net.Mail;
+using System.Net;
 
 namespace BucBoard.Controllers
 {
@@ -23,10 +27,101 @@ namespace BucBoard.Controllers
             return View(_repo.ReadAllAnnouncements());
         }
 
-        public IActionResult Email()
+        [HttpGet]
+        //[AllowAnonymous]
+        public ActionResult Email()
         {
+            
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Email(string studentEmail, string emailSubject, string emailMessage)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderemail = new MailAddress("email@gmail.com", "Test"); // this is the sender, ill make a dummy email instead of what's here
+                    var receiveremail = new MailAddress("chaseday95@gmail.com", "Receiver"); // this is who receives the email, will probably be the professor's email.
+
+                    var password = "demotest123"; // test email's password
+                    var sub = emailSubject + " From: " + studentEmail; //  this is the subject line
+                    var body = emailMessage; // obvious
+
+                    var smtp = new SmtpClient // setup the SMTP client here
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderemail.Address, password)
+                    };
+
+                    using (var mess = new MailMessage(senderemail, receiveremail)
+                    {
+                        Subject = emailSubject,
+                        Body = emailMessage
+                    }
+                    )
+                    {
+                        smtp.Send(mess);
+                    }
+
+                    return View();
+                }
+
+            }
+            catch(Exception)
+            {
+                ViewBag.Error = "Wrench in cogs";
+            }
+
+            return View();
+        }
+        
+
+        //[HttpPost]
+        //[AllowAnonymous]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Email(EmailViewModel model, string returnUrl = null)
+        //{
+        //    ViewData["ReturnUrl"] = returnUrl;
+        //    if (ModelState.IsValid)
+        //    {
+        //        var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
+        //        var result = await _userManager.CreateAsync(user, model.Password);
+        //        if (result.Succeeded)
+        //        {
+        //            _logger.LogInformation("User created a new account with password.");
+
+        //            //Add a user to the default role, or any role we specify 
+        //            await _userManager.AddToRoleAsync(user, "Admin");
+
+        //            //Add the role "Admin" Id to the RolesId column in the AspNetUsers table 
+        //            string role = "c2181b3b-f186-4439-bbba-9a0a68585791";
+
+        //            //Get the users id for the query below
+        //            string Id = await _userManager.GetUserIdAsync(user);
+
+
+
+        //            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        //            var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
+        //            await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+
+        //            await _signInManager.SignInAsync(user, isPersistent: false);
+        //            _logger.LogInformation("User created a new account with password.");
+        //            return RedirectToLocal(returnUrl);
+        //        }
+        //        AddErrors(result);
+        //    }
+
+        //    // If we got this far, something failed, redisplay form
+        //    return View(model);
+        //}
 
         public IActionResult About()
         {
---
+++ BucBoard/BucBoard/Models/ViewModels/EmailViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BucBoard.Models.ViewModels
+{
+    public class EmailViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string StudentEmail { get; set; }
+
+        [Required]
+        [Display(Name = "Subject")]
+        public string Subject { get; set; }
+
+        [Required]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+}
+
